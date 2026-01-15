0abe5c990291bdb2ab59b7976b49027b5ec8be87
MikolajRusin <rusinmiko@gmail.com>
2026-01-15 19:36:13 +0100

    Changed typing

--- dataloader/load_dataset.py
+++ dataloader/load_dataset.py
@@ -12,13 +12,13 @@ class TextLoader(Dataset):
     block_size: int
     device: str = 'cpu'
 
-    def __post_init__(self) -> None:
+    def __post_init__(self):
         self.data = self.tokenizer.encode(self.text)
 
     def __len__(self) -> int:
         return self.data.numel() - self.block_size
 
-    def __getitem__(self, idx: int) -> (torch.tensor, torch.tensor):
+    def __getitem__(self, idx: int) -> (torch.Tensor, torch.Tensor):
         x = self.data[idx:idx+self.block_size]
         y = self.data[idx+1:idx+self.block_size+1]
         x, y = x.to(self.device), y.to(self.device)
--- dataloader/tokenizer.py
+++ dataloader/tokenizer.py
@@ -5,14 +5,14 @@ from dataclasses import dataclass
 class CharTokenizer:
     chars: list
 
-    def __post_init__(self) -> None:
+    def __post_init__(self):
         self.chars = sorted(self.chars)
         self.vocab_size = len(self.chars)
         self.ch2token = {ch: i for i, ch in enumerate(self.chars)}
         self.token2ch = {i: ch for ch, i in self.ch2token.items()}
 
-    def encode(self, text: str) -> torch.tensor:
+    def encode(self, text: str) -> torch.Tensor:
         return torch.tensor([self.ch2token[ch] for ch in text])
 
-    def decode(self, tokens: torch.tensor) -> str:
+    def decode(self, tokens: torch.Tensor) -> str:
         return ''.join([self.token2ch[token.item()] for token in tokens])
